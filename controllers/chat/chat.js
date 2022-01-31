f0ee4f2621589dcea288466826b714ee1072951a
Archan Banerjee <archanbanerjee70@gmail.com>
2022-01-31 13:39:55 +0530

    added fetching messages

--- controllers/chat/chat.js
+++ controllers/chat/chat.js
@@ -1,6 +1,6 @@
 const Chat=require('../../models/Chat');
 
-//create chat
+//Create chat
 exports.createChat=async(req,res)=>{
     try {
         const {user1,user2}=req.body;
@@ -10,6 +10,30 @@ exports.createChat=async(req,res)=>{
             await chat.save();
         }
         if(chat) res.status(201).send(chat._id);
+    } catch (error) {
+        console.log(error);
+        res.status(500).json({error:"Something went wrong!"});
+    }
+}
+
+//Fetch a particular chat
+exports.getAChat=async(req,res)=>{
+    try {
+        const {roomId}=req.params;
+        let chat=await Chat.findOne({_id:roomId});
+        res.status(200).send(chat);
+    } catch (error) {
+        console.log(error);
+        res.status(500).json({error:"Something went wrong!"});
+    }
+}
+
+//Fetch all chats of an artist
+exports.fetchAllChatsOfAnArtist=async(req,res)=>{
+    try {
+        const {artistId}=req.params;
+        let chats=await Chat.find({userIds:{$in:[artistId]}});
+        res.status(200).send(chats);
     } catch (error) {
         console.log(error);
         res.status(500).json({error:"Something went wrong!"});
--- routes/chat/chat.js
+++ routes/chat/chat.js
@@ -1,5 +1,5 @@
 const router=require('express').Router();
-const {createChat}=require('../../controllers/chat/chat');
+const {createChat,getAChat,fetchAllChatsOfAnArtist}=require('../../controllers/chat/chat');
 
 //Create chat
 //Route : '/api/chat/createchat'
@@ -9,5 +9,23 @@ const {createChat}=require('../../controllers/chat/chat');
 //Token : Yes
 
 router.post('/createchat',createChat);
+
+//Get a chat
+//Route : '/api/chat/getachat/:roomId'
+//Method : GET
+//Body : N/A
+//Params : {roomId}
+//Token : Yes
+
+router.get('/getachat/:roomId',getAChat);
+
+//Get all chats of an artist
+//Route : '/api/chat/getallchats/:artistId'
+//Method : GET
+//Body : N/A
+//Params : {artistId}
+//Token : Yes
+
+router.get('/getallchats/:artistId',fetchAllChatsOfAnArtist);
 
 module.exports=router;
