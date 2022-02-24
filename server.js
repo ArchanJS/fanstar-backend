e4bfc1bea8407138cc6378e986c1e07975a56009
Archan Banerjee <archanbanerjee70@gmail.com>
2022-02-24 12:31:45 +0530

    modified

--- server.js
+++ server.js
@@ -55,7 +55,7 @@ const server=app.listen(port,()=>{
 const io=require('socket.io')(server,
     {
         cors: {
-          origin: "http://localhost:3080",
+          origin: "http://localhost:3000",
           methods: ["GET", "POST"]
         }
     });
