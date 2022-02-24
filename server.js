9c481d80a3619526cfbe3ba66672f5afb9cdf4a4
Archan Banerjee <archanbanerjee70@gmail.com>
2022-02-24 12:37:20 +0530

    modified

--- server.js
+++ server.js
@@ -55,7 +55,7 @@ const server=app.listen(port,()=>{
 const io=require('socket.io')(server,
     {
         cors: {
-          origin: "http://localhost:3000",
+          origin: ["http://localhost:3000","https://fanstar.app","https://fanstar-app.netlify.app/"],
           methods: ["GET", "POST"]
         }
     });
